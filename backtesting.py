cf99e7c0f31927d50a1aa377e359f867fb8b9aff
Shrinav <lokashrinav@gmail.com>
2023-06-03 00:33:51 -0400

    Created Class Backtester For Backtesting. Created function nothing, which does nothing as a temporary placeholder

--- backtesting.py
+++ backtesting.py
@@ -1,2 +1,9 @@
 # To obtain historical stock data
 import yfinance as yf 
+
+#Class backtester will be used for backtesting
+#Make sure to change funciton nothing() into a working function
+class Backtester:
+    def nothing():
+        print("does nothing")
+        print("nothing")
