49e1189c6f6e19b526fce4a17bcd86c01f28c151
Shrinav <lokashrinav@gmail.com>
2023-06-03 00:43:45 -0400

    Just added the backtester parameters: Start/End Dates, Initial Capital, Frequency of Trades in def __init__()

--- backtesting.py
+++ backtesting.py
@@ -2,12 +2,15 @@
 import yfinance as yf 
 
 #Class backtester will be used for backtesting
-#Make sure to change funciton nothing() into a working function
-
 class Backtester:
-    def nothing():
-        print("does nothing")
-        print("nothing")
+    def __init__(self, start_date, end_date, initial_capital, frequency_of_trades):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.initial_capital = initial_capital
+        self.freqeuncy_of_trades = frequency_of_trades
+
+
+        
 
 '''Next Steps:
 Define Backtester Parameters Just Like in QuantConnect.
