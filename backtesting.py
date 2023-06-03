f642ff19d0b8c9f41e740715289cf227f5cc9c31
Shrinav <lokashrinav@gmail.com>
2023-06-03 00:38:00 -0400

    Created next steps based on QuantConnect parameters. Planning to add Start/End Dates, Initial Capital, and Frequency of Trades Parameter functions

--- backtesting.py
+++ backtesting.py
@@ -3,7 +3,20 @@ import yfinance as yf
 
 #Class backtester will be used for backtesting
 #Make sure to change funciton nothing() into a working function
+
 class Backtester:
     def nothing():
         print("does nothing")
         print("nothing")
+
+'''Next Steps:
+Define Backtester Parameters Just Like in QuantConnect.
+
+Parameter Examples:
+
+1. Start/End Dates
+2. Initial Capital
+3. Frequency of Trades
+
+
+'''
