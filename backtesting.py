6cace437d211da1d7f8644e27c27192b15bf81f2
Shrinav <lokashrinav@gmail.com>
2023-06-04 00:53:32 -0400

    Created "data" dictionary, which will hold historical stock data. Also created the "import_stock_data()" function for each symbol given in the symbol_list parameter. Noting that the backtester can't use different start_dates/end_dates for each of it's stocks until further improvements can be made.

--- backtesting.py
+++ backtesting.py
@@ -3,23 +3,18 @@ import yfinance as yf
 
 #Class backtester will be used for backtesting
 class Backtester:
-    def __init__(self, start_date, end_date, initial_capital, frequency_of_trades):
+    data = {}
+
+    def __init__(self, start_date, end_date, initial_capital, frequency_of_trades, symbol_list):
         self.start_date = start_date
         self.end_date = end_date
         self.initial_capital = initial_capital
         self.freqeuncy_of_trades = frequency_of_trades
+        self.symbol_list = symbol_list
+    
+    def import_stock_data():
+        for symbol in Backtester.symbol_list:
+            Backtester.data[symbol] = yf.download(symbol, start=Backtester.start_date, end=Backtester.end_date)
+        return Backtester.data
 
 
-        
-
-'''Next Steps:
-Define Backtester Parameters Just Like in QuantConnect.
-
-Parameter Examples:
-
-1. Start/End Dates
-2. Initial Capital
-3. Frequency of Trades
-
-
-'''
