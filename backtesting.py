a3a98ddd830b8eaf566f4f34c7a4a43917d629fd
Shrinav <lokashrinav@gmail.com>
2023-06-04 01:31:02 -0400

    Received an error: Backtester.import_stock_data() takes 0 positional arguments but 1 was given - I realized that I didn't include self in my function, and that I didn't include [symbol] when removing rows with missing values in data[symbol].dropna(). Ran the import_stock_data() function and it worked. Plan to prepare proper output format and strategy implementation next

--- backtesting.py
+++ backtesting.py
@@ -12,18 +12,25 @@ class Backtester:
         self.frequency_of_trades = frequency_of_trades
         self.symbol_list = symbol_list
     
-    def import_stock_data():
+    def import_stock_data(self):
         # A dictionary to store the historical stock data
         data = {}
         # Method to import historical stock data for each symbol in symbol_list
-        for symbol in Backtester.symbol_list:
+        for symbol in self.symbol_list:
             # Downloading historical stock data using yfinance and storing it in the data dictionary based on symbol as key
-            data[symbol] = yf.download(symbol, start=Backtester.start_date, end=Backtester.end_date)
+            data[symbol] = yf.download(symbol, start=self.start_date, end=self.end_date)
             # Checks if any specific value is missing for the symbol
             if data[symbol].isna().any().any():
                 # Outputs Warning Message based on missing data for symbol
                 print(f"Warning: Missing data found for symbol {symbol}")
                 # Removing rows with missing values. Plan to remove specific values later
-                data[symbol] = data.dropna() 
+                data[symbol] = data[symbol].dropna() 
 
-        return Backtester.data
+        return data
+
+bt = Backtester(start_date='2022-01-01', end_date='2022-12-31', initial_capital=100000, frequency_of_trades=10, symbol_list=['AAPL', 'MSFT'])
+
+stock_data = bt.import_stock_data()
+
+print(stock_data)
+    
