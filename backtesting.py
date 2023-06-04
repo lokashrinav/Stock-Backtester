fb97d222d744e49267c47afac743c3dbaac22daf
Shrinav <lokashrinav@gmail.com>
2023-06-04 00:56:34 -0400

    Added Comments for each function in the code for reference sakes. Also the "import_stock_data" function iterates through each symbol in the symbol_list and downloads it's historical data using the yahoo finance library. It returns the "data" dictionary, which contains the historical stock data for all the symbols in symbol_list.

--- backtesting.py
+++ backtesting.py
@@ -1,20 +1,22 @@
-# To obtain historical stock data
-import yfinance as yf 
+# Importing the yfinance library for retrieving historical stock data
+import yfinance as yf
 
-#Class backtester will be used for backtesting
+# Class Backtester will be used for backtesting
 class Backtester:
+    # A dictionary to store the downloaded stock data
     data = {}
 
     def __init__(self, start_date, end_date, initial_capital, frequency_of_trades, symbol_list):
+        # Initializing the Backtester class with the specified parameters
         self.start_date = start_date
         self.end_date = end_date
         self.initial_capital = initial_capital
-        self.freqeuncy_of_trades = frequency_of_trades
+        self.frequency_of_trades = frequency_of_trades
         self.symbol_list = symbol_list
     
     def import_stock_data():
+        # Method to import historical stock data for each symbol in the symbol list
         for symbol in Backtester.symbol_list:
+            # Downloading historical stock data using yfinance and storing it in the data dictionary
             Backtester.data[symbol] = yf.download(symbol, start=Backtester.start_date, end=Backtester.end_date)
         return Backtester.data
-
-
